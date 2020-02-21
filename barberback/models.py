b7a175a3771cc52f060bccc83c7070af5ec80ca8
Macbook <alex@integra24.biz>
2020-02-21 13:22:27 +0700

    create model BarberProfile

--- barberback/models.py
+++ barberback/models.py
@@ -2,10 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class BarberProfile(models.Model):
-    pass
+    
+    class Meta:
+        db_table = 'barber_profile'
+
+    Barber_Phone = models.CharField(max_length=20, default='', blank=True)
+    Barber_User  = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
 
 
